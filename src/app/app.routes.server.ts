32b26f007fe40d94c31908415e8b18e66abe3119
Tasneem-Khalid <tasneemkhalid66@gmail.com>
2025-10-25 20:13:38 +0300

    4th commit

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,16 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'details/:id',
+    renderMode: RenderMode.Server // excludes from prerender
+  },
+  {
+    path: 'checkout/:cartId',
+    renderMode: RenderMode.Server // excludes from prerender
+  },
+  // Add other parameterized routes as needed
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Prerender // catch-all for static routes
   }
 ];
